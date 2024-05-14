c1e50347a300998756f1abbcfffc327103cfb013
Matheus Pedroso <pedroso.matheus2000@gmail.com>
2024-05-14 15:44:21 -0300

    criando o CheckMate

--- ChessUI/obj/Debug/net6.0-windows/ChessUI.AssemblyInfo.cs
+++ ChessUI/obj/Debug/net6.0-windows/ChessUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d354d2adda590aa30c701b2cfbc9e3e7f8aea0e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
